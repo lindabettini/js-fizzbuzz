e69db557e331524f93701532bcb62ce7986009df
lindabettini <linda.bettini@hotmail.it>
2021-12-30 22:25:25 +0100

    Print numbers from 1 to 100 in console

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 console.log('JS OK');
 
-/*
-1.Scrivi un programma che stampi i numeri da 1 a 100,
-2. Per i multipli di 3 stampi Fizz al posto del numero.
-3. Per i multipli di 5 stampi Buzz al posto del numero.
-4. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+/*   
+// 1. Scrivi un programma che stampi i numeri da 1 a 100,
+// 2. Per i multipli di 3 stampi Fizz al posto del numero.
+// 3. Per i multipli di 5 stampi Buzz al posto del numero.
+// 4. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 Prima di partire a scrivere codice poniamoci qualche domanda
 Come faccio a sapere se un numero è divisibile per?
 Abbiamo visto qualcosa di particolare che possiamo usare?% 3 ==0
 % 5 ==0 */
+
+
+// // Scrivi un programma che stampi i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
 
