b5e7133dbbc2516459259fe9424e355ea428e817
lindabettini <linda.bettini@hotmail.it>
2022-01-05 20:01:51 +0100

    Add fizzbuzz

--- script.js
+++ script.js
@@ -7,12 +7,19 @@ console.log('JS OK');
 // 4. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 Prima di partire a scrivere codice poniamoci qualche domanda
 Come faccio a sapere se un numero è divisibile per?
-Abbiamo visto qualcosa di particolare che possiamo usare?% 3 ==0
+Abbiamo visto qualcosa di particolare che possiamo usare?
+% 3 ==0
 % 5 ==0 */
 
 
+let toPrint = document.getElementById('result');
+
+let numbers = '';
+
 // // Scrivi un programma che stampi i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+    console.log(i);    
+    numbers = numbers + i;
 }
 
+toPrint.innerHTML = numbers;
