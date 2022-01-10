114cd6157103edd612c07b2d6554a4caf92220b3
lindabettini <linda.bettini@hotmail.it>
2022-01-10 20:54:40 +0100

    change layout

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
 console.log('JS OK');
 
 /*   
-// 1. Scrivi un programma che stampi i numeri da 1 a 100,
-// 2. Per i multipli di 3 stampi Fizz al posto del numero.
-// 3. Per i multipli di 5 stampi Buzz al posto del numero.
-// 4. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-Prima di partire a scrivere codice poniamoci qualche domanda
-Come faccio a sapere se un numero è divisibile per?
-Abbiamo visto qualcosa di particolare che possiamo usare?
-% 3 ==0
-% 5 ==0 */
+1. Scrivi un programma che stampi i numeri da 1 a 100,
+2. Per i multipli di 3 stampi Fizz al posto del numero.
+3. Per i multipli di 5 stampi Buzz al posto del numero.
+4. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+*/
 
 
-let allNumbers = document.getElementById('any-number');
+let list = document.getElementById('my-list');
 
-let numbers = '';
+let listItems = '';
 
-// // Scrivi un programma che stampi i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-
     if ((i % 3 ==0) && (i % 5 ==0)) {
-        numbers = numbers + `FIZZBUZZ`;
-    } 
-
-    else if (i % 3 ==0) {
-        numbers = numbers + `fizz`;
-    }
-    else if (i % 5 ==0) {
-        numbers = numbers + `buzz`;
-    }
-
-    else if (!(i % 3 ==0) && !(i % 5 ==0)) {
-        numbers = numbers + i;
+        listItems += `<li class="fizzbuzz">FizzBuzz</li>`;
+    } else if (i % 3 ==0) {
+        listItems += `<li class="fizz">Fizz</li>`;
+    } else if (i % 5 ==0) {
+        listItems += `<li class="buzz">Buzz</li>`;
+    } else {
+        listItems += `<li>${i}</li>`;
     }
 }
 
-allNumbers.innerHTML = numbers;
+list.innerHTML = listItems;
 
